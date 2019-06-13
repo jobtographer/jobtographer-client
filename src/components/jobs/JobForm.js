6878aa30936c31e32c638b802c119cf255ce4142
Cosmo Kent <cosmolightfoot@gmail.com>
2019-06-13 14:30:09 -0700

    updated JobForm for location

--- src/components/jobs/JobForm.js
+++ src/components/jobs/JobForm.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function JobForm({ onChange, onSubmit, date, title, company, jobUrl }) {
+function JobForm({ onChange, onSubmit, title, company, jobUrl, location }) {
   return (
     <form onSubmit={onSubmit}>
       <input name="title" value={title} onChange={onChange} />
       <input name="company" value={company} onChange={onChange} />
-      <input name="date" value={date} onChange={onChange} />
       <input name="jobUrl" value={jobUrl} onChange={onChange} />
+      <input name="location" value={location} onChange={onChange} />
       <button>Add Job</button>
     </form>
-  )
+  );
 }
 
 JobForm.propTypes = {
   onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
   company: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
   jobUrl: PropTypes.string.isRequired
 };
 
---
+++ src/containers/AddJob.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import JobForm from '../components/jobs/jobForm';
+
+class AddJob extends PureComponent {
+  static propTypes = {
+    createJob: PropTypes.func.isRequired
+  }
+  state = {
+    title: '',
+    company: '',
+    jobUrl: '',
+    location: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { title, company, jobUrl } = this.state;
+    this.props.createJob({ title, company, jobUrl, location });
+    this.setState({ title: '', company: '', jobUrl: '', location: '' });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { title, company, jobUrl, location } = this.state;
+    return (
+      <JobForm onSubmit={this.onSubmit} onChange={this.onChange} 
+        title={title} company={company} jobUrl={jobUrl} location={location} />
+    );
+  }
+}
+
+const mapDisptachToProps = dispatch =>({
+  // createJob is from static propTypes
+  createJob(job) {
+    // dispatch sends it... newJob is an action
+    dispatch(newJob(job));
+  }
+});
+
+export default connect(
+  null,
+  mapDisptachToProps
+)(AddJob);
+
