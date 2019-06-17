a1f2b179ffe2bd53b19996726a7713f939a8a164
Parker Helland <hellandp@gmail.com>
2019-06-17 13:59:26 -0700

    updated Job form / container to include all fields

--- src/components/jobs/JobForm.js
+++ src/components/jobs/JobForm.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function JobForm({ onChange, onSubmit, title, company, jobUrl, jobLocation }) {
+function JobForm({ onChange, onSubmit, title, company, jobUrl, jobLocation, salary, jobDescriptionText, tracking }) {
   return (
     <form onSubmit={onSubmit}>
       <input placeholder= 'title' name="title" value={title} onChange={onChange} />
       <input placeholder= 'company' name="company" value={company} onChange={onChange} />
       <input placeholder= 'job url' name="jobUrl" value={jobUrl} onChange={onChange} />
       <input placeholder = 'location' name="jobLocation" value={jobLocation} onChange={onChange} />
+      <input placeholder = 'description' name="jobDescriptionText" value={jobDescriptionText} onChange={onChange} />
+      <input placeholder = 'salary' name="salary" value={salary} onChange={onChange} />
+
+      <select name='tracking' onChange={onChange}>
+        <option value={tracking} name="interested">Interested</option>
+        <option value={tracking} name="haveApplied">Applied</option>
+        <option value={tracking} name="phoneInterviewed">Phone Interviewed</option>
+        <option value={tracking} name="technicalInterviewed">Technical Interview</option>
+        <option value={tracking} name="inPersonInterviewed">In Person Interview</option>
+        <option value={tracking} name="jobOffer">Job Offer</option>
+      </select>
+
       <button>Add Job</button>
     </form>
   );
@@ -18,8 +30,11 @@ JobForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
   company: PropTypes.string.isRequired,
-  jobLocation: PropTypes.string.isRequired,
-  jobUrl: PropTypes.string.isRequired
+  jobLocation: PropTypes.string,
+  jobUrl: PropTypes.string,
+  salary: PropTypes.string,
+  jobDescriptionText: PropTypes.string,
+  tracking: PropTypes.string
 };
 
 export default JobForm;
--- src/selectors/sessionSelector.js
+++ src/selectors/sessionSelector.js
@@ -1,1 +1,2 @@
 export const getToken = state => state.session.token;
+export const getSessionId = state => state.session.id;
